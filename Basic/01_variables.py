f8e4ae2b8a00a6fc804542a99949504a5e268bea
BDeLa23 <bieldelatorrerodriguez@gmail.com>
2025-11-03 16:50:26 +0100

    Add variables and print statements in 01_variables.py

---
+++ Basic/01_variables.py
@@ -0,0 +1,14 @@
+### Variables ###
+
+nombre = "Pepe"
+apellido = "Juan"
+edad = 50
+
+print(nombre)
+print(apellido)
+print(edad)
+
+### Print + variable ###
+
+variable = "Pepe Juan."
+print("Hola, " + variable)
