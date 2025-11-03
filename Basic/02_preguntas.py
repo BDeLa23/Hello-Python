66277908288ae89aa7b97d976b80fc8cf0f3ca68
BDeLa23 <bieldelatorrerodriguez@gmail.com>
2025-11-03 16:55:52 +0100

    Add user input prompts and help message

---
+++ Basic/02_preguntas.py
@@ -0,0 +1,21 @@
+### Preguntas ###
+
+nombre = input("Cual es tu nombre? ")
+apellido = input("Cual es tu apellido? ")
+edad = input("Cual es tu edad? ")
+
+'''
+input(""):
+La función input("") en
+Python sirve para preguntar
+cosas por ejemplo: sobre el.
+
+Para más preguntas
+sobre esto enviar
+un mensaje a
+#ayuda en discord
+'''
+
+print(nombre)
+print(apellido)
+print(edad)
