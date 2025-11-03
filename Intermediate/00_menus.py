1869ecfa72261ae62fb10559c8d83b77983621c6
BDeLa23 <bieldelatorrerodriguez@gmail.com>
2025-11-03 17:53:03 +0100

    Create 00_menus.py

---
+++ Intermediate/00_menus.py
@@ -0,0 +1,21 @@
+import os
+
+def menu():
+  while True:
+    print("---MENÚ---")
+    print("1) Opción 1")
+    print("2) Opción 2")
+    print("3) Opción 3")
+
+    opcion = input("Selecciona una opción: ")
+
+    if opcion == "1":
+      print("Hola.")
+
+    elif opcion == "2":
+      print("Pepe")
+
+    elif opcion == "3":
+      print("Juan")
+
+menu()
