482580aec81cd2c996cd3a4f5b9f7e8a7b8b2361
BDeLa23 <bieldelatorrerodriguez@gmail.com>
2025-11-05 20:32:38 +0100

    Add basic arithmetic operations in 02_operaciones.py
    This script performs basic arithmetic operations: addition, subtraction, multiplication, and division.

---
+++ Basic/02_operaciones.py
@@ -0,0 +1,13 @@
+### Operaciones ###
+
+print(2 + 2) # Suma
+print(2 - 2) # Resta
+print(2 * 2) # Multiplicación
+print(2 / 2) # División
+
+'''
+Este codigo lo que hara es decirle
+al Python que sume los numeros que
+le indiques. En el resultado os
+saldrá la respuesta directa.
+'''
