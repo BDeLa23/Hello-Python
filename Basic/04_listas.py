6fc322007285c9c4689f8cb9ad6dcc53b2f3214f
BDeLa23 <bieldelatorrerodriguez@gmail.com>
2025-11-22 08:20:48 +0100

    Create 04_listas.py to illustrate list concepts
    Add a Python script demonstrating list usage with comments.

---
+++ Basic/04_listas.py
@@ -0,0 +1,20 @@
+'''
+Las listas son variables
+pero si comienzas con corcheras
+y pones los números tienen un
+orden. (Tmabién se puede hacer 
+con texto)
+'''
+
+my_list = [35, 24, 62, 52, 30, 30, 17] # Esto es una lista
+print(my_list[0])
+
+'''
+Las listas se imprimen
+con el nombre de la 
+variable y dentro de las corcheras
+el número en orden que quieres
+que se imprima. En las listas
+el orden es diferente, el orden
+empieza por el valor [0]
+'''
