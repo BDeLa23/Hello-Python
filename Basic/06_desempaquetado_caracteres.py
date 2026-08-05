556a0742042c951a8384b02df1a820025d5895f7
BDeLa23 <bieldelatorrerodriguez@gmail.com>
2026-08-05 07:32:36 +0200

    Actualizar 06_desempaquetado_caracteres.py

--- Basic/06_desempaquetado_caracteres.py
+++ Basic/06_desempaquetado_caracteres.py
@@ -1,6 +1,7 @@
 language = "python" # Le indicamos un string. (puede tener el nombre que quieras)
 a, b, c, d, e, f = language # Le ponemos los caractes contenidos de forma alfabética (se puede hacer de otra forma) y le indicamos en que variable tiene que hacer la acción.
 
-print(a, b, c, d, e, f) # Le decimos que imprima el primer carácter del string ubicado en la variable language
+print(a) # Le decimos que imprima el primer carácter del string ubicado en la variable language
 print(e) # Le decimos que imprima el quinto carácter del string ubicado en la variable language
 # Este paso se puede hacer con otros caracteres
+print(a, b, c, d, e, f) # Le decimos que imprima todos los caracteres en un sólo print.
